e6f9da77b9d293a18fbf657016f48791497ee157
Alexander Erbiste <alexander.erbiste@gmail.com>
2025-03-19 22:20:11 -0300

    calculadora atualizada com classes

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,58 +1,26 @@
 ﻿using System;
+using Etec.FP.Negocios;
 
 class Program
 {
     static void Main(string[] args)
     {
-        double num1, num2, resultado;
-        string operacao;
+        Calculadora Calc1 = new Calculadora();
 
-        Console.WriteLine("Calculadora - Operações Básicas");
+        Console.Write("10 + 10 = ");
+        Console.Write(Calc1.Somar(10, 10));
 
-        Console.Write("Digite o primeiro número: ");
-        num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("10 - 10 = ");
+        Console.Write(Calc1.Subtrair(10, 10));
 
-        Console.Write("Digite a operação (+, -, *, /): ");
-        operacao = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("10 * 10 = ");
+        Console.Write(Calc1.Multiplicar(10, 10));
 
-        Console.Write("Digite o segundo número: ");
-        num2 = Convert.ToDouble(Console.ReadLine());
-
-        switch (operacao)
-        {
-            case "+":
-                resultado = num1 + num2;
-                Console.WriteLine($"Resultado: {num1} + {num2} = {resultado}");
-                break;
-
-            case "-":
-                resultado = num1 - num2;
-                Console.WriteLine($"Resultado: {num1} - {num2} = {resultado}");
-                break;
-
-            case "*":
-                resultado = num1 * num2;
-                Console.WriteLine($"Resultado: {num1} * {num2} = {resultado}");
-                break;
-
-            case "/":
-                if (num2 != 0)
-                {
-                    resultado = num1 / num2;
-                    Console.WriteLine($"Resultado: {num1} / {num2} = {resultado}");
-                }
-                else
-                {
-                    Console.WriteLine("Erro: Divisão por zero não permitida.");
-                }
-                break;
-
-            default:
-                Console.WriteLine("Operação inválida! Tente novamente com +, -, * ou /.");
-                break;
-        }
-
-        Console.WriteLine("Pressione qualquer tecla para sair...");
-        Console.ReadKey();
+        Console.WriteLine();
+        Console.Write("10 / 10 =");
+        Console.Write(Calc1.Dividir(10, 10));
+    
     }
 }
